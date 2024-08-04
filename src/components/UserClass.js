e648f4575eefdbc0f2de5ee80440ed6550be383e
OmKeshri <omkeshri21@gmail.com>
2024-08-04 13:55:12 +0530

    day 8

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,26 +5,51 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        count : 0,
-        count2 : 1
+      count: 0,
+      count2: 1,
+
+      userInfo:{
+        name: "om",
+        location: "us",
+        avatar_url: "http://dummy-photo.com",
+      }
     };
   }
 
+  async componentDidMount(){
+    const data = await fetch("https://api.github.com/users/omkeshri")
+    const json = await data.json();
+    console.log(json)
+
+    this.setState({
+        userInfo: json
+    })
+  }
+
+  componentDidUpdate(){
+    console.log("component did update");
+  }
+
+  componentWillUnmount(){
+    console.log("component will unmount")
+  }
+
   render() {
-    const { name, location } = this.props;
+    // const { name, location } = this.props;
+    const{name, location, avatar_url} = this.state.userInfo;
     return (
       <div className="user-card">
         <h1>{this.state.count}</h1>
-        <button onClick={() => {
-            this.setState(
-                {count: this.state.count+1}
-            )
-        }}>
-            Count Increase
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          Count Increase
         </button>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
-        <h3>Contact: </h3>
+        <img src={avatar_url}></img>
       </div>
     );
   }
